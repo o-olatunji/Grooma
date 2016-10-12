99327aee2a776d72c052f7138a206c010dc4b37d
Olamide Olatunji <olatunji14@gmail.com>
2016-10-12 11:57:57 -0700

    commit

--- VetsNow/VetsNow/SelectGroomerTableViewController.swift
+++ VetsNow/VetsNow/SelectGroomerTableViewController.swift
@@ -11,7 +11,7 @@ import Parse
 
 class SelectGroomerTableViewController: UITableViewController {
     
-   
+    
     let main = OperationQueue.main
     var images:[UIImage] = []
     var names: [String] = []
@@ -23,78 +23,56 @@ class SelectGroomerTableViewController: UITableViewController {
         
     }
     
-        func retrieveMessages() {
-            var userArray: [String] = []
-            var query:PFQuery = PFQuery(className: "User")
-            var currentUser = query.whereKey("username", equalTo: PFUser.current())
-            currentUser.findObjectsInBackground {
-                (objects, error) -> Void in
-                
-                for object in objects! {
-                    let name:String? = (object as PFObject)["name"] as? String
-                    if name != nil {
-                        self.names.append(name!)
-                    }
+    func retrieveMessages() {
+        var query:PFQuery = PFQuery(className: "_User")
+        query.findObjectsInBackground {
+            (objects, error) -> Void in
+            
+            for object in objects! {
+                let name:String? = (object as PFObject)["name"] as? String
+                if name != nil {
+                    self.names.append(name!)
+                    
+                    print("name added")
                 }
-                self.tableView.reloadData()
             }
-        }
-    
-
-
-
-
-
-    
-   /* func getImages(objects:[PFObject]) {
-        
-        for object in objects {
-            
-            let thumbNail = object["ProfilePicure"] as! PFFile
-            
-            thumbNail.getDataInBackground(block: { (data, error) in
-                
-                if error == nil {
-                    
-                    let image = UIImage(data:data!)
-                    //image object implementation
-                    self.images.append(image!)
-                    
-                    print("image added!")
-                }
-                
-                self.main.addOperation {
-                    self.tableView.reloadData()
-                    
-                    print("it works!")
-                }
-                
-            })
-            
+            self.tableView.reloadData()
         }
     }
     
-    func getNames() {
-        let query = PFQuery(className:"User")
-        query.findObjectsInBackground { (objects, error) -> Void in
-            if error == nil {
-                for object in objects! {
-                    
-                    self.names.append(object["name"] as! String)
-                    
-                }
-                self.main.addOperation {
-                    self.tableView.reloadData()
-                }
-            }
+    
+    func getImages() {
+        var query:PFQuery = PFQuery(className: "_User")
+        query.findObjectsInBackground {
+            (objects, error) -> Void in
             
-            else {
-                print(error)
+            for object in objects! {
+                
+                let thumbNail = object["ProfilePicure"] as! PFFile
+                
+                thumbNail.getDataInBackground(block: { (data, error) in
+                    
+                    if error == nil {
+                        
+                        let image = UIImage(data:data!)
+                        //image object implementation
+                        self.images.append(image!)
+                        
+                        print("image added!")
+                    }
+                    
+                    self.main.addOperation {
+                        self.tableView.reloadData()
+                        
+                        print("it works!")
+                    }
+                    
+                })
+                
             }
         }
         
-        
-    } */
+    }
     
     
     @IBAction func cancelButtonClicked(_ sender: AnyObject) {
@@ -107,14 +85,15 @@ class SelectGroomerTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return images.count
+        return names.count
+        
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "groomers", for: indexPath) as! SelectGroomerCell
         
-        // cell.vendorProfilePic.image = images[indexPath.row]
-     cell.companyName.text = names[indexPath.row]
+        cell.vendorProfilePic.image = images[indexPath.row]
+        cell.companyName.text = names[indexPath.row]
         
         return cell
         
